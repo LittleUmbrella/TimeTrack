21a76b6837648a7ca7c6a45970ef4681354a8d37
Toby Farris <tfarris@rei.com>
2020-04-12 19:32:36 -0700

    refactoring to modularize

--- time.js
+++ time.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
+const addTimeEntry = require('./lib/time');
 
-//const timeFile = fs.openSync("~/documents/time.txt");
-const filepath = "/Users/tfarris/Documents/time.txt";
-const data = new Date().getDate()
-fs.appendFileSync(filepath, `\n${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}` );
-fs.appendFileSync(filepath, `\n${process.argv[2]}`, "utf8");
+addTimeEntry(process.argv[2], "/Users/tfarris/Documents/time.txt");
