e88ba51cfa1fad6c8cb0b2241e2b00bf2078fe56
Toby Farris <tfarris@bardydx.com>
2020-05-08 01:28:27 -0700

    normalize across windows and posix oses

--- time.js
+++ time.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
 const addTimeEntry = require('./lib/time');
+const pathToTimeEntryLog = path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], 'time.txt');
 
-addTimeEntry(process.argv[2], process.argv[3] || "/Users/tfarris/Documents/time.txt");
+fs.stat(pathToTimeEntryLog, function(err, stat) {
+    if(err == null) {
+        console.log('File exists');
+    } else if(err.code === 'ENOENT') {
+        // file does not exist
+        console.log(`File (${pathToTimeEntryLog}) DOES NOT exist. Creating...`);
+        fs.writeFileSync(pathToTimeEntryLog, '');
+    } else {
+        console.log('Some other error: ', err.code);
+        return;
+    }
+    addTimeEntry(process.argv[2], process.argv[3] || `${pathToTimeEntryLog}`);
+});
+
--- track.js
+++ track.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+const path = require('path');
 const lineParser = require('./lib/lineParser');
 const processLineByLine = require('./lib/lineProcessor');
+const pathToTimeEntryLog = path.join(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'], 'time.txt');
 
-lineParser(processLineByLine, process.argv[3] || "/Users/tfarris/Documents/time.txt");
+lineParser(processLineByLine, process.argv[3] || pathToTimeEntryLog);
